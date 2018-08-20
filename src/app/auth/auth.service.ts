03db72eec2d2f84d042597b8d4737b8e5893a5b0
Mark Churcher <mark.churcher@gmail.com>
2018-08-20 14:05:35 +1200

    auth token persisted to local storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -28,6 +28,12 @@ export class AuthService {
     return this.isAuthenticated;
   }
 
+  setAuthTimer(durationSeconds: number) {
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, durationSeconds * 1000);
+  }
+
   createUser(email: string, password: string) {
     const authData: AuthData = {
       email: email,
@@ -50,14 +56,32 @@ export class AuthService {
      this.token = token;
      if (token) {
        const expiresInSeconds = res.expiresIn;
-       this.tokenTimer = setTimeout(() => {
-         this.logout();
-       }, expiresInSeconds * 1000);
+       this.setAuthTimer(expiresInSeconds);
         this.isAuthenticated = true;
         this.authStatusListner.next(true);
+        const now = new Date();
+        const expirationDate = new Date(now.getTime() + expiresInSeconds * 1000);
+        this.saveAuthData(token, expirationDate);
         this.router.navigate(['/']);
      }
     });
+  }
+
+  autoAuthUser() {
+    const authInfo = this.getAuthData();
+    if (!authInfo) {
+      return;
+    }
+    const now = new Date();
+    const expiresInSeconds = (authInfo.expirationDate.getTime() - now.getTime()) / 1000;
+    if (expiresInSeconds > 0) {
+      console.log('you already logged in and have token');
+      this.token = authInfo.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresInSeconds);
+      this.authStatusListner.next(true);
+      console.log(expiresInSeconds);
+    }
   }
 
   logout() {
@@ -66,7 +90,29 @@ export class AuthService {
     this.isAuthenticated = false;
     this.authStatusListner.next(false);
     clearTimeout(this.tokenTimer);
+    this.clearAuthData();
     this.router.navigate(['/login']);
   }
 
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expirationDate', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expirationDate');
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate)
+    };
+  }
 }
